bf3e092daee401a6db052f240f479598afb92d9c
thamika-git <adheesha@raditha.com>
2021-11-01 09:12:55 +0530

    Set up the timer.

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,53 @@ import ReactDOM from "react-dom";
 
 import ButtonGroup from "./components/ButtonGroup";
 
-function App() {
-    return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <h1 className="text-9xl lg:text-12xl font-black text-gray-800">
-                15:00
-            </h1>
-            <ButtonGroup />
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            timeLeft: 900,
+            timerRunning: true,
+            controlText: "Start",
+        };
+
+        this.tick = this.tick.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.state.timerRunning) {
+            this.setState({ controlText: "Pause" });
+            this.timerID = setInterval(this.tick, 1000);
+        }
+    }
+
+    tick() {
+        if (this.state.timeLeft) {
+            this.setState(state => ({ timeLeft: state.timeLeft - 1 }));
+        } else {
+            clearInterval(this.timerID);
+        }
+    }
+
+    formatTime() {
+        let minutes = String(Math.floor(this.state.timeLeft / 60));
+        let formattedMinutes = minutes.length === 2 ? minutes : `0${minutes}`;
+
+        let seconds = String(this.state.timeLeft % 60);
+        let formattedSeconds = seconds.length === 2 ? seconds : `0${seconds}`;
+
+        return `${formattedMinutes}:${formattedSeconds}`;
+    }
+
+    render() {
+        return (
+            <div className="flex flex-col justify-center items-center h-screen">
+                <h1 className="text-9xl lg:text-12xl font-black text-gray-800">
+                    {this.formatTime()}
+                </h1>
+                <ButtonGroup controlText={this.state.controlText} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
